a2fda01f6f1dbb66b19bc284d87ea4ef2635b16e
Gisell0511 <giseorozco05@gmail.com>
2024-09-05 21:42:34 -0500

    let

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ const category2=document.getElementById("s2");
 const category3=document.getElementById("s3");
 const notepad=document.querySelector("notepad");
 const add_btn=document.getElementById("add_note");
+
+let message_text= "Noted Saved Sucessfully";
+let appendNote= document.createElement("div");
+notepad.appendChild(appendNote);
+
+let cat1= "Diseño";
+let cat2= "Networking";
+let cat3="Student";
+
+const date= new Date();
+
+const day= date.getDate();
+const month= date.getMonth();
+const year= date.getFullYear();
+console.log(day);
+
+const montNames= ["Enero", "Febrero", "Marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+const d = new Date();
+let fullmonth = montNamesd[d.getMonth()]
+console.log(fullmonth + '' + year);
+
+var days=["lunes", "martes", "mmiercoles", "jueves", "viernes", "sabado", "domingo"];
+var day2= days[date.getDay()];
+console.log(day2);
+
+window.onbeforeunload = function() {
+    return true;
+}
